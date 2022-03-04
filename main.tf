3d74f69d07dd5be5961fda9b190653de2de4e30d
Greg Foletta <greg@foletta.org>
2022-03-04 11:57:21 +1100

    Rename ftnt_sites module to aws_ftnt_sites

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-module "ftnt_sites" {
+module "aws_ftnt_sites" {
     sites = var.sites   
     providers = { aws = aws.apse2 }
-    source = "./ftnt_sites"
+    source = "./aws_ftnt_sites"
 }
 
 output "rsa_private_key" { 
-    value = module.ftnt_sites.rsa_private_key
+    value = module.aws_ftnt_sites.rsa_private_key
     sensitive = true
 }
